8fcb62a3d489be193dca7b85e5d023d72c1269f6
Harivardhan05 <harivardhan.palla@gmail.com>
2025-09-23 17:11:21 +0530

    Data validation completed

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -9,3 +9,12 @@ In my class I don't have any functions. I just need to have some variables defin
 class DataIngestionArtifact:
     trained_file_path:str
     test_file_path:str
+
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
