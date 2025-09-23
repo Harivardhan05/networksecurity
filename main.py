862635eb2380e71556fea7cd1146b118cae0ad28
Harivardhan05 <harivardhan.palla@gmail.com>
2025-09-24 01:20:05 +0530

    Model training completed

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from networksecurity.entity.config_entity import TrainingPipelineConfig
 import logging
 import sys
 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
 if __name__ == "__main__":
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
@@ -31,6 +34,12 @@ if __name__ == "__main__":
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
         
+        logging.info("Model Training stared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
         
     except Exception as e:
         raise NetworkSecurityException(e, sys)
